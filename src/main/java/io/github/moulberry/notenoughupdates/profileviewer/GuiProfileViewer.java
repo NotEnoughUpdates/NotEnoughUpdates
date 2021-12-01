d9d8de26d609a9df2c3fc9d23963bd2fd6e462cc
nopothegamer <40329022+nopothegamer@users.noreply.github.com>
2021-12-01 17:36:01 +1100

    comments

--- src/main/java/io/github/moulberry/notenoughupdates/profileviewer/GuiProfileViewer.java
+++ src/main/java/io/github/moulberry/notenoughupdates/profileviewer/GuiProfileViewer.java
@@ -201,11 +201,13 @@ public class GuiProfileViewer extends GuiScreen {
                         0, 100 / 200f, 0, 20 / 185f, GL11.GL_NEAREST);
                 Utils.drawStringCenteredScaledMaxWidth(profileId, Minecraft.getMinecraft().fontRendererObj, guiLeft + 50,
                         guiTop + sizeY + 3 + 10, true, 90, new Color(63, 224, 208, 255).getRGB());
+                //ironmoon icon
                 if (currProfileInfo != null && currProfileInfo.has("game_mode") && currProfileInfo.get("game_mode").getAsString().equals("ironman")) {
                     GlStateManager.color(1, 1, 1, 1);
                     Minecraft.getMinecraft().getTextureManager().bindTexture(pv_ironman);
                     Utils.drawTexturedRect(guiLeft - 16 - 5, guiTop + sizeY + 5, 16, 16, GL11.GL_NEAREST);
                 }
+                //bingo! icon
                 if (currProfileInfo != null && currProfileInfo.has("game_mode") && currProfileInfo.get("game_mode").getAsString().equals("bingo")) {
                     GlStateManager.color(1, 1, 1, 1);
                     Minecraft.getMinecraft().getTextureManager().bindTexture(pv_bingo);
