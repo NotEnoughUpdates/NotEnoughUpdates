aa05d4a2d794c0d78bf907882c4df299b3ee13b4
Walker Selby <git@walkerselby.com>
2022-03-27 13:16:28 -0700

    NEU Pack Dev Additions Add "getmob" subcommand Add "getmobs" subcommand Add "getarmorstand" subcommand Add "getarmorstands" subcommand Add optional radius argument for neupackdev subcommands. (If not specified, it defaults to the prior static 5 block radius) Added changes to changelog
    These additions will be a good utility for pack developers, as they can get information about NBT data of certain mobs and armor stands that they might not be able to texture otherwise, such as the ID of a held sword or armor pieces.

--- src/main/java/io/github/moulberry/notenoughupdates/commands/dev/PackDevCommand.java
+++ src/main/java/io/github/moulberry/notenoughupdates/commands/dev/PackDevCommand.java
@@ -8,6 +8,9 @@ import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.item.EntityArmorStand;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
@@ -20,40 +23,273 @@ public class PackDevCommand extends ClientCommandBase {
 
 	@Override
 	public void processCommand(ICommandSender sender, String[] args) throws CommandException {
-		if (args.length == 1 && args[0].equalsIgnoreCase("getnpc")) {
+		if (args.length >= 1) {
 			double distSq = 25;
-			EntityPlayer closestNPC = null;
 			EntityPlayerSP p = Minecraft.getMinecraft().thePlayer;
-			for (EntityPlayer player : Minecraft.getMinecraft().theWorld.playerEntities) {
-				if (player instanceof AbstractClientPlayer && p != player && player.getUniqueID().version() != 4) {
-					double dSq = player.getDistanceSq(p.posX, p.posY, p.posZ);
-					if (dSq < distSq) {
-						distSq = dSq;
-						closestNPC = player;
-					}
+
+			if (args.length == 2) {
+				try {
+					distSq = Double.parseDouble(args[1]) * Double.parseDouble(args[1]);
+				} catch (NumberFormatException e) {
+					sender.addChatMessage(new ChatComponentText(
+						EnumChatFormatting.RED + "Invalid distance! Must be a number, defaulting to a radius of 5."));
 				}
 			}
+			sender.addChatMessage(new ChatComponentText(
+				EnumChatFormatting.RED + "Debug: distSq = " + distSq));
 
-			if (closestNPC == null) {
-				Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(
-					EnumChatFormatting.RED + "No NPCs found within 5 blocks :("));
-			} else {
-				Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(
-					EnumChatFormatting.GREEN + "Copied entity texture id to clipboard"));
-				MiscUtils.copyToClipboard(((AbstractClientPlayer) closestNPC)
-					.getLocationSkin()
-					.getResourcePath()
-					.replace("skins/", ""));
+			switch (args[0].toLowerCase()) {
+				case "getnpc":
+					EntityPlayer closestNPC = null;
+					for (EntityPlayer player : Minecraft.getMinecraft().theWorld.playerEntities) {
+						if (player instanceof AbstractClientPlayer && p != player && player.getUniqueID().version() != 4) {
+							double dSq = player.getDistanceSq(p.posX, p.posY, p.posZ);
+							if (dSq < distSq) {
+								distSq = dSq;
+								closestNPC = player;
+							}
+						}
+					}
+
+					if (closestNPC == null) {
+						sender.addChatMessage(new ChatComponentText(
+							EnumChatFormatting.RED + "No NPCs found within " + (Double.parseDouble(args[1])) + " blocks. :("));
+					} else {
+						sender.addChatMessage(new ChatComponentText(
+							EnumChatFormatting.GREEN + "Copied NPC entity texture id to clipboard"));
+						MiscUtils.copyToClipboard(((AbstractClientPlayer) closestNPC)
+							.getLocationSkin()
+							.getResourcePath()
+							.replace("skins/", ""));
+					}
+					break;
+
+				case "getmob":
+					Entity closestMob = null;
+					for (Entity mob : Minecraft.getMinecraft().theWorld.loadedEntityList) {
+						if (mob != null && mob != Minecraft.getMinecraft().thePlayer && mob instanceof EntityLiving) {
+							double dSq = mob.getDistanceSq(p.posX, p.posY, p.posZ);
+							if (dSq < distSq) {
+								distSq = dSq;
+								closestMob = mob;
+							}
+						}
+					}
+
+					if (closestMob == null) {
+						sender.addChatMessage(new ChatComponentText(
+							EnumChatFormatting.RED + "No mobs found within" + (Double.parseDouble(args[1])) + " blocks. :("));
+					} else {
+						sender.addChatMessage(new ChatComponentText(
+							EnumChatFormatting.GREEN + "Copied mob data to clipboard"));
+
+						MiscUtils.copyToClipboard(mobDataBuilder(closestMob).toString());
+
+					}
+					break;
+
+				case "getmobs":
+					String mobData;
+					StringBuilder mobStringBuilder = new StringBuilder();
+					for (Entity mob : Minecraft.getMinecraft().theWorld.loadedEntityList) {
+						if (mob != null && mob != Minecraft.getMinecraft().thePlayer && mob instanceof EntityLiving &&
+							mob.getDistanceSq(p.posX, p.posY, p.posZ) < distSq) {
+							mobStringBuilder.append(mobDataBuilder(mob));
+						}
+					}
+					mobData = mobStringBuilder.toString();
+					if (mobData.equals("")) {
+						sender.addChatMessage(new ChatComponentText(
+							EnumChatFormatting.RED + "No mobs found within" + (Double.parseDouble(args[1])) + " blocks. :("));
+					} else {
+						sender.addChatMessage(new ChatComponentText(
+							EnumChatFormatting.GREEN + "Copied mob data to clipboard"));
+						MiscUtils.copyToClipboard(mobData);
+					}
+					break;
+
+				case "getarmorstand":
+					EntityArmorStand closestArmorStand = null;
+					for (Entity armorStand : Minecraft.getMinecraft().theWorld.loadedEntityList) {
+						if (armorStand instanceof EntityArmorStand) {
+							double dSq = armorStand.getDistanceSq(p.posX, p.posY, p.posZ);
+							if (dSq < distSq) {
+								distSq = dSq;
+								closestArmorStand = (EntityArmorStand) armorStand;
+							}
+						}
+					}
+					
+					if (closestArmorStand == null) {
+						sender.addChatMessage(new ChatComponentText(
+							EnumChatFormatting.RED + "No armor stands found within " + (Double.parseDouble(args[1])) + " blocks. :("));
+					} else {
+						sender.addChatMessage(new ChatComponentText(
+							EnumChatFormatting.GREEN + "Copied armor stand data to clipboard"));
+						
+						MiscUtils.copyToClipboard(armorStandDataBuilder(closestArmorStand).toString());
+							
+					}
+					break;
+
+				case "getarmorstands":
+					String armorStandData;
+					StringBuilder armorStandStringBuilder = new StringBuilder();
+					for (Entity armorStand : Minecraft.getMinecraft().theWorld.loadedEntityList) {
+						if (armorStand instanceof EntityArmorStand &&
+							armorStand.getDistanceSq(p.posX, p.posY, p.posZ) < distSq) {
+							armorStandStringBuilder.append(armorStandDataBuilder((EntityArmorStand) armorStand));
+						}
+					}
+					armorStandData = armorStandStringBuilder.toString();
+					if (armorStandData.equals("")) {
+						sender.addChatMessage(new ChatComponentText(
+							EnumChatFormatting.RED + "No armor stands found within" + (Double.parseDouble(args[1])) + " blocks. :("));
+					} else {
+						sender.addChatMessage(new ChatComponentText(
+							EnumChatFormatting.GREEN + "Copied armor stand data to clipboard"));
+						MiscUtils.copyToClipboard(armorStandData);
+					}
+					break;
+
+				default:
+					break;
 			}
-			return;
-		}
-		NotEnoughUpdates.INSTANCE.packDevEnabled = !NotEnoughUpdates.INSTANCE.packDevEnabled;
-		if (NotEnoughUpdates.INSTANCE.packDevEnabled) {
-			Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(
-				EnumChatFormatting.GREEN + "Enabled pack developer mode."));
+
 		} else {
-			Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(
-				EnumChatFormatting.RED + "Disabled pack developer mode."));
+			NotEnoughUpdates.INSTANCE.packDevEnabled = !NotEnoughUpdates.INSTANCE.packDevEnabled;
+			if (NotEnoughUpdates.INSTANCE.packDevEnabled) {
+				sender.addChatMessage(new ChatComponentText(
+					EnumChatFormatting.GREEN + "Enabled pack developer mode."));
+			} else {
+				sender.addChatMessage(new ChatComponentText(
+					EnumChatFormatting.RED + "Disabled pack developer mode."));
+			}
 		}
+	}
+
+	public StringBuilder mobDataBuilder(Entity mob) {
+		StringBuilder mobData = new StringBuilder();
+
+		mobData
+			.append("Entity Id: ")
+			.append(mob.getEntityId())
+			.append("\nMob: ")
+			.append(mob.getName());
+
+		//Preventing null pointers, checking armor slots and hand to see if item is present.
+		if (((EntityLiving) mob).getHeldItem() != null) {
+			mobData
+				.append("\nItem: ")
+				.append(((EntityLiving) mob).getHeldItem())
+				.append("\nItem Display Name: ")
+				.append(((EntityLiving) mob).getHeldItem().getDisplayName())
+				.append("\nItem Tag Compound: ")
+				.append(((EntityLiving) mob).getHeldItem().getTagCompound().toString())
+				.append("\nItem Tag Compound Extra Attributes: ")
+				.append(((EntityLiving) mob).getHeldItem().getTagCompound().getTag("ExtraAttributes"));
+		} else {
+			mobData.append("\nItem: null");
+		}
+
+		if (((EntityLiving) mob).getCurrentArmor(0) != null) {
+			mobData
+				.append("\nBoots: ")
+				.append(((EntityLiving) mob).getCurrentArmor(0).getTagCompound());
+		} else {
+			mobData.append("\nBoots: null");
+		}
+
+		if (((EntityLiving) mob).getCurrentArmor(1) != null) {
+			mobData
+				.append("\nLeggings: ")
+				.append(((EntityLiving) mob).getCurrentArmor(1).getTagCompound());
+		} else {
+			mobData.append("\nLeggings: null");
+		}
+
+		if (((EntityLiving) mob).getCurrentArmor(2) != null) {
+			mobData
+				.append("\nChestplate: ")
+				.append(((EntityLiving) mob).getCurrentArmor(2).getTagCompound());
+		} else {
+			mobData.append("\nChestplate: null");
+		}
+
+		if (((EntityLiving) mob).getCurrentArmor(3) != null) {
+			mobData
+				.append("\nHelmet: ")
+				.append(((EntityLiving) mob).getCurrentArmor(3).getTagCompound());
+		} else {
+			mobData.append("\nHelmet: null");
+		}
+		mobData.append("\n\n");
+		return mobData;
+	}
+
+	public StringBuilder armorStandDataBuilder(EntityArmorStand armorStand) {
+		StringBuilder armorStandData = new StringBuilder();
+
+		armorStandData
+			.append("Entity Id: ")
+			.append(armorStand.getEntityId())
+			.append("\nMob: ")
+			.append(armorStand.getName())
+			.append("\nCustom Name: ")
+			.append(armorStand.getCustomNameTag());
+
+		//Preventing null pointers, checking armor slots and hand to see if item is present.
+		if (armorStand.getHeldItem() != null) {
+			armorStandData
+				.append("\nItem: ")
+				.append(armorStand.getHeldItem())
+				.append("\nItem Display Name: ")
+				.append(armorStand.getHeldItem().getDisplayName());
+			if (armorStand.getHeldItem().getTagCompound() != null) {
+				armorStandData
+					.append("\nItem Tag Compound: ")
+					.append(armorStand.getHeldItem().getTagCompound().toString())
+					.append("\nItem Tag Compound Extra Attributes: ")
+					.append(armorStand.getHeldItem().getTagCompound().getTag("ExtraAttributes"));
+			} else {
+				armorStandData.append("\nItem Tag Compound: null");
+			}
+		} else {
+			armorStandData.append("\nItem: null");
+		}
+
+		if (armorStand.getCurrentArmor(0) != null) {
+			armorStandData
+				.append("\nBoots: ")
+				.append(armorStand.getCurrentArmor(0).getTagCompound());
+		} else {
+			armorStandData.append("\nBoots: null");
+		}
+
+		if (armorStand.getCurrentArmor(1) != null) {
+			armorStandData
+				.append("\nLeggings: ")
+				.append(armorStand.getCurrentArmor(1).getTagCompound());
+		} else {
+			armorStandData.append("\nLeggings: null");
+		}
+
+		if (armorStand.getCurrentArmor(2) != null) {
+			armorStandData
+				.append("\nChestplate: ")
+				.append(armorStand.getCurrentArmor(2).getTagCompound());
+		} else {
+			armorStandData.append("\nChestplate: null");
+		}
+
+		if (armorStand.getCurrentArmor(3) != null) {
+			armorStandData
+				.append("\nHelmet: ")
+				.append(armorStand.getCurrentArmor(3).getTagCompound());
+		} else {
+			armorStandData.append("\nHelmet: null");
+		}
+		armorStandData.append("\n\n");
+		return armorStandData;
 	}
 }
