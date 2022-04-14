68b7b3183ff7086770ce3271645a5abe7f39500b
nea <romangraef@gmail.com>
2022-04-14 17:21:58 +0200

    NPE

--- src/main/java/io/github/moulberry/notenoughupdates/util/ItemUtils.java
+++ src/main/java/io/github/moulberry/notenoughupdates/util/ItemUtils.java
@@ -9,12 +9,17 @@ import java.util.List;
 
 public class ItemUtils {
 	public static void appendLore(ItemStack is, List<String> moreLore) {
-		NBTTagCompound display = is.getTagCompound().getCompoundTag("display");
+		NBTTagCompound tagCompound = is.getTagCompound();
+		if(tagCompound == null){
+			tagCompound = new NBTTagCompound();
+		}
+		NBTTagCompound display = tagCompound.getCompoundTag("display");
 		NBTTagList lore = display.getTagList("Lore", 8);
 		for (String s : moreLore) {
 			lore.appendTag(new NBTTagString(s));
 		}
 		display.setTag("Lore", lore);
-		is.getTagCompound().setTag("display", display);
+		tagCompound.setTag("display", display);
+		is.setTagCompound(tagCompound);
 	}
 }
