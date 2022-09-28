8a96e1a834fc7f15474dff730dfb5163de4abe68
nea <romangraef@gmail.com>
2022-09-29 01:15:45 +0200

    Use api selected variable to find best profile

--- src/main/java/io/github/moulberry/notenoughupdates/profileviewer/ProfileViewer.java
+++ src/main/java/io/github/moulberry/notenoughupdates/profileviewer/ProfileViewer.java
@@ -825,6 +825,10 @@ public class ProfileViewer {
 								}
 
 								String cuteName = profile.get("cute_name").getAsString();
+								if (profile.has("selected") && profile.get("selected").getAsBoolean()) {
+									lastCuteName = cuteName;
+									break;
+								}
 								if (lastCuteName == null) lastCuteName = cuteName;
 								profileNames.add(cuteName);
 								if (member.has("last_save")) {
