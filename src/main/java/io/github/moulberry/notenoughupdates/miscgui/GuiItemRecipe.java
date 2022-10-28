08d6696c36b116f9fe537b767fa23e57afe87cbc
nea <romangraef@gmail.com>
2022-10-29 01:15:19 +0200

    Make recipe tabs work again
    why do people always break my code :sob:

--- src/main/java/io/github/moulberry/notenoughupdates/miscgui/GuiItemRecipe.java
+++ src/main/java/io/github/moulberry/notenoughupdates/miscgui/GuiItemRecipe.java
@@ -278,7 +278,7 @@ public class GuiItemRecipe extends GuiScreen {
 		super.mouseClicked(mouseX, mouseY, mouseButton);
 		NeuRecipe currentRecipe = getCurrentRecipe();
 		int[] topLeft = currentRecipe.getPageFlipPositionLeftTopCorner();
-		ArrowPagesUtils.onPageSwitchMouse(
+		if (ArrowPagesUtils.onPageSwitchMouse(
 			guiLeft,
 			guiTop,
 			topLeft,
@@ -286,7 +286,22 @@ public class GuiItemRecipe extends GuiScreen {
 			getCurrentRecipeList().size(),
 			pageChange ->
 				changeRecipe(currentTab, pageChange)
-		);
+		)) return;
+
+		for (int i = 0; i < tabs.size(); i++) {
+			if (isWithinRect(
+				mouseX - guiLeft,
+				mouseY - guiTop,
+				TAB_POS_X,
+				TAB_POS_Y + TAB_OFFSET_Y * i,
+				TAB_SIZE_X,
+				TAB_SIZE_Y
+			)) {
+				changeRecipe(i, currentIndex);
+				Utils.playPressSound();
+				return;
+			}
+		}
 
 		for (RecipeSlot slot : getAllRenderedSlots()) {
 			if (isWithinRect(mouseX, mouseY, slot.getX(this), slot.getY(this), SLOT_SIZE, SLOT_SIZE)) {
