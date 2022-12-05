c6509537c2678d729fccce20c2fc2262391f3fa4
heyngra <heyngra.wspolpraca@gmail.com>
2022-12-05 22:30:19 +0100

    check treasures every tick and not render

--- src/main/java/io/github/moulberry/notenoughupdates/miscfeatures/world/FrozenTreasuresHighlighter.java
+++ src/main/java/io/github/moulberry/notenoughupdates/miscfeatures/world/FrozenTreasuresHighlighter.java
@@ -33,14 +33,19 @@ import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class FrozenTreasuresHighlighter extends GenericBlockHighlighter {
 
 	private static final FrozenTreasuresHighlighter INSTANCE = new FrozenTreasuresHighlighter();
 
 	public static FrozenTreasuresHighlighter getInstance() {return INSTANCE;}
+
+	private final Set<Entity> highlightedTreasures = new HashSet<>();
 
 	@Override
 	protected boolean isEnabled() {
@@ -55,19 +60,25 @@ public class FrozenTreasuresHighlighter extends GenericBlockHighlighter {
 		Block b = w.getBlockState(key).getBlock();
 		return b == Blocks.ice;
 	}
-	
+
+	@SubscribeEvent
+	public void onTick(TickEvent.ClientTickEvent event) {
+		if (event.phase != TickEvent.Phase.END||!isEnabled()) return;
+		List<Entity> entities = Minecraft.getMinecraft().theWorld.getLoadedEntityList();
+		highlightedTreasures.removeIf(it -> !(it instanceof EntityArmorStand) || ((EntityArmorStand) it).getCurrentArmor(3)==null);
+		for (Entity e : entities) {
+			if ((e instanceof EntityArmorStand) && ((EntityArmorStand) e).getCurrentArmor(3)!=null) highlightedTreasures.add(e);
+		}
+	}
 	@SubscribeEvent
 	public void onWorldRenderLast(RenderWorldLastEvent event) {
 		if (!isEnabled()) return;
 		World w = Minecraft.getMinecraft().theWorld;
 		if (w == null) return;
-		List<Entity> entities = w.getLoadedEntityList();
-		for (Entity e : entities) {
-			if (!(e instanceof EntityArmorStand) || ((EntityArmorStand) e).getCurrentArmor(3)==null) continue;
-			BlockPos blockPos = e.getPosition();
-			BlockPos blockPos2 = blockPos.add(0, 1, 0);
-			if (tryRegisterInterest(blockPos2)) {
-				RenderUtils.renderBoundingBox(blockPos2, getColor(blockPos2), event.partialTicks);
+		for (Entity e : highlightedTreasures) {
+			BlockPos blockPos = e.getPosition().add(0, 1, 0);
+			if (tryRegisterInterest(blockPos)) {
+				RenderUtils.renderBoundingBox(blockPos, getColor(blockPos), event.partialTicks);
 			}
 		}
 	}
