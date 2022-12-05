ad39550ccd4963ddc88c056c9ef07abdbad083f7
heyngra <heyngra.wspolpraca@gmail.com>
2022-12-05 23:30:56 +0100

    Update FrozenTreasuresHighlighter.java

--- src/main/java/io/github/moulberry/notenoughupdates/miscfeatures/world/FrozenTreasuresHighlighter.java
+++ src/main/java/io/github/moulberry/notenoughupdates/miscfeatures/world/FrozenTreasuresHighlighter.java
@@ -20,7 +20,6 @@
 package io.github.moulberry.notenoughupdates.miscfeatures.world;
 
 import io.github.moulberry.notenoughupdates.NotEnoughUpdates;
-import io.github.moulberry.notenoughupdates.core.util.render.RenderUtils;
 import io.github.moulberry.notenoughupdates.util.SBInfo;
 import io.github.moulberry.notenoughupdates.util.SpecialColour;
 import net.minecraft.block.Block;
@@ -29,15 +28,10 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityArmorStand;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockPos;
-import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
-import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class FrozenTreasuresHighlighter extends GenericBlockHighlighter {
 
@@ -45,7 +39,6 @@ public class FrozenTreasuresHighlighter extends GenericBlockHighlighter {
 
 	public static FrozenTreasuresHighlighter getInstance() {return INSTANCE;}
 
-	private final Set<Entity> highlightedTreasures = new HashSet<>();
 
 	@Override
 	protected boolean isEnabled() {
@@ -62,35 +55,17 @@ public class FrozenTreasuresHighlighter extends GenericBlockHighlighter {
 	}
 
 	@SubscribeEvent
-	public void onTick(TickEvent.ClientTickEvent event) {
+	public void onTickNew(TickEvent.ClientTickEvent event) {
 		if (event.phase != TickEvent.Phase.END||!isEnabled()) return;
+
 		List<Entity> entities = Minecraft.getMinecraft().theWorld.getLoadedEntityList();
-		highlightedTreasures.removeIf(it -> !(it instanceof EntityArmorStand)
-			|| ((EntityArmorStand) it).getCurrentArmor(3)==null || isValidHighlightSpot(it.getPosition().add(0,1,0)));
 		for (Entity e : entities) {
-			if ((e instanceof EntityArmorStand) && ((EntityArmorStand) e).getCurrentArmor(3)!=null) highlightedTreasures.add(e);
-		}
-	}
-	@SubscribeEvent
-	public void onWorldRenderLast(RenderWorldLastEvent event) {
-		if (!isEnabled()) return;
-		World w = Minecraft.getMinecraft().theWorld;
-		if (w == null) return;
-		for (Entity e : highlightedTreasures) {
-			BlockPos blockPos = e.getPosition().add(0, 1, 0);
-			if (tryRegisterInterest(blockPos)) {
-				RenderUtils.renderBoundingBox(blockPos, getColor(blockPos), event.partialTicks);
-			}
+			if ((e instanceof EntityArmorStand) && ((EntityArmorStand) e).getCurrentArmor(3)!=null) highlightedBlocks.add(e.getPosition().add(0,1,0));
 		}
 	}
 
 	@Override
 	protected int getColor(BlockPos blockPos) {
 		return SpecialColour.specialToChromaRGB(NotEnoughUpdates.INSTANCE.config.world.frozenTreasuresColor);
-	}
-
-	public boolean tryRegisterInterest(BlockPos blockPos) {
-		boolean canSee = canPlayerSeeNearBlocks(blockPos.getX(), blockPos.getY(), blockPos.getZ());
-		return isValidHighlightSpot(blockPos) && canSee;
 	}
 }
