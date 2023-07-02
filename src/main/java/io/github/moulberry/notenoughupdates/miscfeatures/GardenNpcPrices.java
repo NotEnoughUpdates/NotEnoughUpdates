dfb263db7bf37b7147f0c8570f363314eb5606b0
heyngra <heyngra.wspolpraca@gmail.com>
2023-07-02 18:02:40 +0200

    use repo instead of enum
    requires https://github.com/NotEnoughUpdates/NotEnoughUpdates-REPO/pull/952

--- src/main/java/io/github/moulberry/notenoughupdates/miscfeatures/GardenNpcPrices.java
+++ src/main/java/io/github/moulberry/notenoughupdates/miscfeatures/GardenNpcPrices.java
@@ -19,6 +19,8 @@
 
 package io.github.moulberry.notenoughupdates.miscfeatures;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import io.github.moulberry.notenoughupdates.NotEnoughUpdates;
 import io.github.moulberry.notenoughupdates.autosubscribe.NEUAutoSubscribe;
 import io.github.moulberry.notenoughupdates.util.ItemResolutionQuery;
@@ -35,47 +37,13 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 @NEUAutoSubscribe
 public class GardenNpcPrices {
-	private enum SkyMartItem {
-
-		GARDEN_SCYTHE("GARDEN_SCYTHE", EnumChatFormatting.BLUE+"Garden Scythe", 20, true),
-		BUILDERS_RULER("BUILDERS_RULER", EnumChatFormatting.GOLD+"Builder's Ruler", 20, true),
-		SUNDER("SUNDER;1", EnumChatFormatting.BLUE+"Sunder I", 10, false),
-		DEDICATION("DEDICATION;1", EnumChatFormatting.BLUE+"Dedication", 250, false),
-		GREEN_THUMB("GREEN_THUMB;1", EnumChatFormatting.BLUE+"Green Thumb", 1500, false),
-		LOTUS_BRACELET("LOTUS_BRACELET", EnumChatFormatting.BLUE+"Lotus Bracelet", 50, false),
-		LOTUS_BELT("LOTUS_BELT", EnumChatFormatting.BLUE+"Lotus Belt", 100, false),
-		LOTUS_NECKLACE("LOTUS_NECKLACE", EnumChatFormatting.BLUE+"Lotus Necklace", 200, false),
-		LOTUS_CLOAK("LOTUS_CLOAK", EnumChatFormatting.BLUE+"Lotus Cloak", 500, false),
-		YELLOW_BANDANA("YELLOW_BANDANA", EnumChatFormatting.BLUE+"Yellow Bandana", 300, false),
-		BASIC_GARDENING_HOE("BASIC_GARDENING_HOE", EnumChatFormatting.GREEN+"Basic Gardening Hoe", 5, true),
-		ADVANCED_GARDENING_HOE("ADVANCED_GARDENING_HOE", EnumChatFormatting.BLUE+"Advanced Gardening Hoe", 25, true),
-		BASIC_GARDENING_AXE("BASIC_GARDENING_AXE", EnumChatFormatting.GREEN+"Basic Gardening Axe", 5, true),
-		ADVANCED_GARDENING_AXE("ADVANCED_GARDENING_AXE", EnumChatFormatting.BLUE+"Advanced Gardening Axe", 25, true),
-		SKYMART_BROCHURE("SKYMART_BROCHURE", EnumChatFormatting.BLUE+"SkyMart Brochure", 100, false),
-		LARGE_WALNUT("LARGE_WALNUT", EnumChatFormatting.BLUE+"Large Walnut", 150, false),
-		HIVE_BARN_SKIN("HIVE_BARN_SKIN", EnumChatFormatting.GOLD+"Hive Barn Skin", 10000, false),
-		TRADING_POST_BARN_SKIN("TRADING_POST_BARN_SKIN", EnumChatFormatting.GREEN+"Trading Post Barn Skin", 1000, false),
-		AUTUMN_HUT_BARN_SKIN("AUTUMN_HUT_BARN_SKIN", EnumChatFormatting.GREEN+"Autumn Hut Barn Skin", 2000, false),
-		CASTLE_BARN_SKIN("CASTLE_BARN_SKIN", EnumChatFormatting.GOLD+"Castle Barn Skin", 15000, false),
-		BAMBOO_BARN_SKIN("BAMBOO_BARN_SKIN", EnumChatFormatting.DARK_PURPLE+"Bamboo Barn Skin", 7500, false),
-		;
-		private final String internalName;
-		private final String displayName;
-		private final int copperPrice;
-		private final boolean isWorthless;
-		SkyMartItem(String internalName, String displayName, int copperPrice, boolean isWorthless) {
-			this.internalName = internalName;
-			this.displayName = displayName;
-			this.copperPrice = copperPrice;
-			this.isWorthless = isWorthless;
-		}
-	}
 	private final Pattern itemRegex = Pattern.compile("§5§o §.([a-zA-Z \\-]+)(?:§8x(\\d+))?");
 	//§5§o §aEnchanted Cactus Green §8x421
 	//§5§o §aEnchanted Hay Bale §8x62
@@ -124,13 +92,16 @@ public class GardenNpcPrices {
 	}
 
 	public Object[] calculateCopper(int amount) {
+		JsonObject skymart = Utils.getConstant("skymart", NotEnoughUpdates.INSTANCE.manager.gson);
 		Map<String, Double> prices = new HashMap<>();
-		for (SkyMartItem item : SkyMartItem.values()) {
-			if (item.isWorthless && NotEnoughUpdates.INSTANCE.config.tooltipTweaks.ignoreBeginnerItems) continue;
-			boolean isBazaar = NotEnoughUpdates.INSTANCE.manager.auctionManager.getBazaarInfo(item.internalName)!=null;
+		for (Map.Entry<String, JsonElement> entry : skymart.entrySet()) {
+			String internalName = entry.getKey();
+			JsonObject item = entry.getValue().getAsJsonObject();
+			if (!Objects.equals(item.get("currency").getAsString(), "copper") || (item.get("isWorthless").getAsBoolean() && NotEnoughUpdates.INSTANCE.config.tooltipTweaks.ignoreBeginnerItems)) continue;
+			boolean isBazaar = NotEnoughUpdates.INSTANCE.manager.auctionManager.getBazaarInfo(internalName)!=null;
 			if (!isBazaar&&NotEnoughUpdates.INSTANCE.config.tooltipTweaks.ignoreAHItems) continue;
-			double price = NotEnoughUpdates.INSTANCE.manager.auctionManager.getBazaarOrBin(item.internalName, false) /item.copperPrice*amount;
-			prices.put(item.displayName, price);
+			double price = NotEnoughUpdates.INSTANCE.manager.auctionManager.getBazaarOrBin(internalName, false) /item.get("price").getAsInt()*amount;
+			prices.put(item.get("display").getAsString(), price);
 		}
 		Map.Entry<String, Double> maxPrice = Collections.max(prices.entrySet(), Map.Entry.comparingByValue());
 		return new Object[]{maxPrice.getKey(), maxPrice.getValue()};
