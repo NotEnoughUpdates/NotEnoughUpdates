305aa1516f2b3807ebc651e166232cc9f202adc6
NopoTheGamer <40329022+NopoTheGamer@users.noreply.github.com>
2023-10-07 17:09:57 +1100

    im not sure what the else part is doing but seems to work
    Co-authored-by: Linnea Gräf <roman.graef@gmail.com>

--- src/main/java/io/github/moulberry/notenoughupdates/profileviewer/level/task/SlayingTaskLevel.java
+++ src/main/java/io/github/moulberry/notenoughupdates/profileviewer/level/task/SlayingTaskLevel.java
@@ -100,15 +100,13 @@ public class SlayingTaskLevel extends GuiTaskLevel {
 				HashMap.class
 			);
 
-			for (int i = 0; i < 2; i++) {
-				masterCompMap.forEach((floor, value) -> {
-					if (allComps.containsKey(floor)) {
-						allComps.put(floor, allComps.get(floor) + value);
-					} else {
-						allComps.put(floor, value);
-					}
-				});
-			}
+			masterCompMap.forEach((floor, value) -> {
+				if (allComps.containsKey(floor)) {
+					allComps.put(floor, allComps.get(floor) + value * 2);
+				} else {
+					allComps.put(floor, value);
+				}
+			});
 		}
 		// THIS SERVER IS AWESOME I LOVE CONSISTENCY!!!!!!!
 
