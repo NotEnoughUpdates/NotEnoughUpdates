288e080b715978c23616414431fe615501c6c1a1
IlmarsXd <ilmars500@gmail.com>
2023-12-20 01:04:03 +0200

    Fixed ConcurrentModificationException in Texture Manager caused by other mods and NEU loading a player on a different thread

--- src/main/java/io/github/moulberry/notenoughupdates/mixins/MixinTextureManager.java
+++ src/main/java/io/github/moulberry/notenoughupdates/mixins/MixinTextureManager.java
@@ -19,20 +19,40 @@
 
 package io.github.moulberry.notenoughupdates.mixins;
 
+import com.google.common.collect.Lists;
 import io.github.moulberry.notenoughupdates.miscfeatures.ItemCustomizeManager;
+import net.minecraft.client.renderer.texture.ITickable;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.util.ResourceLocation;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Collections;
+import java.util.List;
+
 @Mixin(TextureManager.class)
 public class MixinTextureManager {
+
+	@Mutable
+	@Final
+	@Shadow
+	private List<ITickable> listTickables;
+
 	@Inject(method = "bindTexture", at = @At("HEAD"), cancellable = true)
 	public void bindTexture(ResourceLocation location, CallbackInfo ci) {
 		if (ItemCustomizeManager.disableTextureBinding) {
 			ci.cancel();
 		}
+	}
+
+	// Fixes ConcurrentModificationException in TextureManager#tick (with 5zig but probably other mods too)
+	@Inject(method = "<init>", at = @At("RETURN"))
+	public void constructor(CallbackInfo ci) {
+		listTickables = Collections.synchronizedList(Lists.newArrayList());
 	}
 }
