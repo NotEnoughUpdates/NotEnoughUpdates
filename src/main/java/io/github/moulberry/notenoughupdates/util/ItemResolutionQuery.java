c66d42ab7479c29104c179a4c8f713bb09e5d84a
jani270 <jani270@gmx.de>
2024-04-15 17:45:58 +0200

    Fixed null things and water bottle + awkward potion

--- src/main/java/io/github/moulberry/notenoughupdates/util/ItemResolutionQuery.java
+++ src/main/java/io/github/moulberry/notenoughupdates/util/ItemResolutionQuery.java
@@ -344,10 +344,15 @@ public class ItemResolutionQuery {
 		String potion = getExtraAttributes().getString("potion");
 		int potionLvl = getExtraAttributes().getInteger("potion_level");
 		String potionName = getExtraAttributes().getString("potion_name");
+		String potionType = getExtraAttributes().getString("potion_type");
 		if (potionName != null && !potionName.isEmpty()) {
 			return "POTION_" + potionName.toUpperCase(Locale.ROOT) + ";" + potionLvl;
-		} else {
+		} else if (potion != null && !potion.isEmpty()) {
 			return "POTION_" + potion.toUpperCase(Locale.ROOT) + ";" + potionLvl;
+		} else if (potionType != null && !potionType.isEmpty()) {
+			return "POTION_" + potionType.toUpperCase(Locale.ROOT);
+		} else {
+			return "WATER_BOTTLE";
 		}
 	}
 
